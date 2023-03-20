7443532a57ea8ecb17ed13740ef947a40a179811
unknown <kamila.wilczynska92@gmail.com>
2023-03-20 21:24:57 +0000

    Fix handleSubmit

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -4,21 +4,22 @@ import styles from './PickCity.module.scss';
 
 import { useState } from 'react';
 
-const PickCity = ({ action, ...props }) => {
+const PickCity = ({ action}) => {
   const [city, setCity] = useState('');
 
  const handleSubmit = (e) => {
     e.preventDefault();
     
-	action({city });
+	action(city);
+	setCity('');
   };
 
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={handleSubmit}> 
       <label>
         <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
       </label>
-      <Button onSubmit={handleSubmit}>Search</Button>
+      <Button >Search</Button>
     </form>
   );
 };
